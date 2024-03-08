49441266d9fe6d93c474dbf28dd09e82f968ca12
Dan Habot <dan.habot@gmail.com>
2024-03-08 11:43:21 -0500

    added parallel with no return

--- workers/parallel.go
+++ workers/parallel.go
@@ -17,3 +17,19 @@ func Parallel[IN, OUT any](in <-chan IN, out chan<- OUT, count int, process func
 
 	wg.Wait()
 }
+
+func Parallel0[IN any](in <-chan IN, count int, process func(in IN)) {
+	var wg sync.WaitGroup
+	wg.Add(count)
+
+	for i := 0; i < count; i++ {
+		go func() {
+			defer wg.Done()
+			for e := range in {
+				process(e)
+			}
+		}()
+	}
+
+	wg.Wait()
+}
