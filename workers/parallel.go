30f411277e4578cf4d78bc7f068f34019dbdc87e
Dan Habot <dan.habot@gmail.com>
2024-06-02 01:02:16 -0400

    deleted code

--- workers/parallel.go
+++ workers/parallel.go
@@ -3,19 +3,9 @@ package workers
 import "sync"
 
 func Parallel[IN, OUT any](in <-chan IN, out chan<- OUT, count int, process func(in IN) OUT) {
-	var wg sync.WaitGroup
-	wg.Add(count)
-
-	for i := 0; i < count; i++ {
-		go func() {
-			defer wg.Done()
-			for e := range in {
-				out <- process(e)
-			}
-		}()
-	}
-
-	wg.Wait()
+	Parallel0[IN](in, count, func(in IN) {
+		out <- process(in)
+	})
 }
 
 func Parallel0[IN any](in <-chan IN, count int, process func(in IN)) {
